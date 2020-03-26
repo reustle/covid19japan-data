be2fd43911f17cfd8a2e858b6c6593d0cce6404b
Alastair Tse <alastair@liquidx.net>
2020-03-26 18:49:02 +0900

    Fix prefecture data missing

--- generate.js
+++ generate.js
@@ -2,13 +2,14 @@ const moment = require('moment')
 
 const FetchPatientData = require('./src/fetch_patient_data.js')
 const FetchDailySummary = require('./src/fetch_daily_summary.js')
+const FetchPrefectureSummary = require('./src/fetch_prefecture_summary.js')
 const Summarize = require('./src/summarize.js')
 
 
 const fetchAndSummarize = async (dateString) => {
   const patients = await FetchPatientData.fetchPatientData(`./docs/patient_data/${dateString}.json`)
   const daily = await FetchDailySummary.fetchDailySummary()
-  const prefectures = await FetchDailySummary.fetchDailySummary()
+  const prefectures = await FetchPrefectureSummary.fetchPrefectureSummary()
   
   Summarize.summarize(patients, daily, prefectures, `./docs/summary/${dateString}.json`)
 }
