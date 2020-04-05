3a3ad4b255bd1b34a375f82cca4f6de3b13f9f10
Alastair Tse <alastair@liquidx.net>
2020-04-05 10:51:01 +0900

    Remove LastUpdated dependency

--- generate.js
+++ generate.js
@@ -4,7 +4,6 @@ const moment = require('moment')
 const FetchPatientData = require('./src/fetch_patient_data.js')
 const FetchDailySummary = require('./src/fetch_daily_summary.js')
 const FetchPrefectureSummary = require('./src/fetch_prefecture_summary.js')
-const FetchLastUpdated = require('./src/fetch_last_updated.js')
 const Summarize = require('./src/summarize.js')
 
 const generateLastUpdated = async (patients) => {
@@ -47,7 +46,7 @@ const fetchAndSummarize = async (dateString) => {
 
   // Work out lastUpdated.
   const lastUpdated = await generateLastUpdated(patients)
-  console.log(`Last updated: ${lastUpdated}`)
+  console.log(`Last updated timestamp: ${lastUpdated}`)
 
   // Write patient data
   const patientOutputFilename = `./docs/patient_data/${dateString}.json`
