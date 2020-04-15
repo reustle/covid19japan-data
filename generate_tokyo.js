96dcd1e37c6e4766bca89ed2425609f4acd2ff72
Alastair Tse <alastair@liquidx.net>
2020-04-15 18:24:08 +0900

    Debugging

--- generate_tokyo.js
+++ generate_tokyo.js
@@ -6,6 +6,7 @@ const FetchTokyoCases = require('./src/fetch_tokyo_counts.js')
 const generateTokyoCounts = () => {
   FetchTokyoCases.fetchTokyoCounts()
     .then(tokyoCases => {
+      console.log(`Read ${tokyoCases.length} Tokyo wards/cities.`)
       if (tokyoCases.length > 0) {
         const filename = `./docs/tokyo/counts.json`
         fs.writeFileSync(filename, JSON.stringify(tokyoCases, null, '  '))
