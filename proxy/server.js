3fd7e0db820a1f146dab32796da92fca5e6be746
Alastair Tse <alastair@liquidx.net>
2020-04-16 18:45:34 +0900

    Add firebase build things for proxy

---
+++ proxy/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const proxy = require('./functions/proxy.js')
+
+let port = process.env.PORT || 3999
+
+app.get('/proxy', (req, res) => {
+  proxy.fetch(req, res)
+})
+
+var listener = app.listen(port, function () {
+  console.log('listening on port ' + listener.address().port);
+  console.log('http://localhost:' + listener.address().port);
+});
