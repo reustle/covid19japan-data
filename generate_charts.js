a17c4cc9d1f1acdc42141db7092b696974a948e2
Alastair Tse <alastair@liquidx.net>
2020-05-01 17:20:43 +0900

    Generate all prefecture and daily charts.

--- generate_charts.js
+++ generate_charts.js
@@ -8,10 +8,51 @@ const dailySummaries = summary.daily
 
 // generate summary for confirmed 
 const historyLength = 60
-const confirmedValues = _.slice(_.map(summary.daily, o => { return { date: o.date, value:o.confirmedAvg7d } }), summary.daily.length - historyLength)
 const deceasedValues = charts.rollingAverage(
   _.slice(_.map(summary.daily, o => { return { date: o.date, value:o.deceased } }), summary.daily.length - historyLength),
   7, 
   'value')
-const svg = charts.svgSparklineWithData(deceasedValues, 400, 80)
-console.log(svg)
+//const svg = charts.svgSparklineWithData(deceasedValues, 400, 80)
+
+const drawLineChart = (values, name) => {
+  const svg = charts.svgSparklineWithData(values, 400, 80)
+  fs.writeFileSync(`./docs/charts/${name}.svg`, svg)
+}
+
+const drawBarChart = (values, name, maxY) => {
+  const svg = charts.svgBarChartWithData(values, 400, 80, maxY)
+  fs.writeFileSync(`./docs/charts/${name}.svg`, svg)
+}
+
+const drawPrefectureBarCharts = (prefectureSummaries, duration) => {
+  let maxY = 0
+  for (let prefecture of prefectureSummaries) {
+    let prefectureMax = _.max(prefecture.dailyConfirmedCount)
+    if (prefectureMax > maxY) {
+      maxY = prefectureMax
+    }
+  }
+
+  for (let prefecture of prefectureSummaries) {
+    let name = _.camelCase(prefecture.name)
+    let values = _.slice(prefecture.dailyConfirmedCount, prefecture.dailyConfirmedCount.length - duration)
+    let avgValues = charts.rollingAverage(_.map(values, o => { return {value: o}}), 7, 'value')
+    drawBarChart(avgValues, name, maxY)
+  }
+}
+
+const drawCumulativeLineCharts = (dailySummaries, duration) => {
+  let confirmedValues = _.slice(_.map(dailySummaries, o => { return { date: o.date, value:o.confirmedAvg7d } }), dailySummaries.length - duration)
+  console.log(confirmedValues)
+  drawLineChart(confirmedValues, 'confirmedDailyAvg')
+
+  let dailyCharts = ['deceased', 'recovered', 'critical']
+  for (let chartValue of dailyCharts) {
+    let values = _.map(dailySummaries, o => { return { date: o.date, value:o[chartValue] } })
+    values = _.slice(charts.rollingAverage(values, 7), values.length - duration)
+    drawLineChart(values, `${chartValue}DailyAvg`)
+  }
+}
+
+drawPrefectureBarCharts(summary.prefectures, 30)
+drawCumulativeLineCharts(summary.daily, 60)
--- src/charts.js
+++ src/charts.js
@@ -75,7 +75,7 @@ const svgSparklineWithData = (values, width, height) => {
   return d3n.svgString()
 }
 
-const svgSparkBarChartWithData = (values, width, height) => {
+const svgBarChartWithData = (values, width, height, maxY) => {
   const d3n = new D3Node()      // initializes D3 with container element
   const d3 = d3n.d3
   const margin =  {top: 10, right: 10, bottom: 10, left: 10}
@@ -85,45 +85,33 @@ const svgSparkBarChartWithData = (values, width, height) => {
 
   var parseTime = d3.timeParse("%Y-%m-%d");
   values.forEach(d => { d.date = parseTime(d.date); d.value = +d.value })
-  values = movingAverage(values, )
 
-  var x = d3.scaleTime().range([0, chartWidth]);
+  if (!maxY) {
+    maxY = d3.max(values, function(d) { return d.value; })
+  }
+
+  var x = d3.scaleBand().range([0, chartWidth]);
   var y = d3.scaleLinear().range([chartHeight, 0])
 
-  x.domain(d3.extent(values, function(d) { return d.date; }));
-  y.domain([0, d3.max(values, function(d) { return d.value; })]);
-
-  var line = d3.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.value); })
-    .curve(d3.curveMonotoneX);
-
-  var area = d3.area()
-    .x(function(d) { return x(d.date); })
-    .y0(chartHeight)
-    .y1(function(d) { return y(d.value); })
-    .curve(d3.curveMonotoneX)
+  x.domain(d3.range(values.length)).padding(0.01);
+  y.domain([0, maxY]);
 
   var svg = d3n.createSVG(width, height)
     .append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
 
-  svg.append("path")
-    .data([values])
-    .attr("class", "area")
-    .attr('fill', 'rgba(0, 0, 0, 0.2)')
-    .attr("d", area);
-
-  svg.append('path')
-    .data([values])
-    .attr('class', 'line')
-    .attr("stroke", "black")
-    .attr("fill", "none")
-    .attr('d', line)
+  svg.selectAll('rect')
+    .data(values)
+    .enter().append('rect')
+    .attr('class', 'bar')
+    .attr('x', (d, i) => x(i))
+    .attr('width', x.bandwidth() - 2)
+    .attr('y', d=> y(d.value))
+    .attr('height', d => y(0) - y(d.value))
 
   return d3n.svgString()  
 }
 
 exports.svgSparklineWithData = svgSparklineWithData
-exports.svgSparkBarChartWithData = svgSparkBarChartWithData
+exports.svgBarChartWithData = svgBarChartWithData
 exports.rollingAverage = rollingAverage
