112f9b243df7f52f6166c8ed024a506cfbcc830c
Alastair Tse <alastair@liquidx.net>
2020-07-27 10:20:08 +0900

    Fix issue with query not being set in POST

--- proxy/functions/status.js
+++ proxy/functions/status.js
@@ -6,11 +6,19 @@ exports.setStatus = async (request, response) => {
   if (request.query.name) {
     name = request.query.name
   }
+  if (request.body.name) {
+    name = request.body.name
+  }
 
   let status = 'blank'
   if (request.query.status) {
     status = request.query.status
   }
+  if (request.body.status) {
+    status = request.body.status
+  }
+
+  console.log({status: status, name: name})
 
   const db = admin.firestore();
   const docRef = db.collection('status').doc(name);
