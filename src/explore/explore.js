2e26d920b58e521b7d54c674f4246da2b1d76027
Alastair Tse <alastair@liquidx.net>
2020-08-23 17:32:16 +0900

    data explorer now can switch between cumulative and daily.

--- src/explore/explore.js
+++ src/explore/explore.js
@@ -1,11 +1,15 @@
 import _ from 'lodash'
 import moment from 'moment'
 import { div, span, tr, td, el } from 'quickelement'
+import Summary from '../summarize.js'
+
+
 
 const th = (klass, attributes, contents) => { return el('th', klass, attributes, contents ) } 
 
 const _state = {
-  maxDaysBefore: 30,
+  maxDaysBefore: 60,
+  countMethod: 'cumulative',
   selectedField: 'confirmed',
   patients: []
 }
@@ -22,16 +26,16 @@ const render = () => {
   const tableHead = table.querySelector('thead')
   const tableBody = table.querySelector('tbody')
 
-  console.log(_state.prefectureSummary)
-
   tableHead.innerHTML = ''
   let columnHeaders = [
     th('', 'Prefecture'),
     th('', 'Total'),
   ]
-  if (_state.selectedField == 'confirmed' || _state.selectedField == 'deceased') {
+
+  const fieldWithHistory = ['confirmed', 'deceased', 'recovered']
+
+  if (fieldWithHistory.indexOf(_state.selectedField) != -1) {
     let dates = _.map(_.range(0, _state.maxDaysBefore), i => { return moment().subtract(i, 'days').format('M/DD') })
-    console.log(_.range(0, _state.maxDaysBefore))
     columnHeaders = _.concat(columnHeaders, _.map(dates, date => th('', date)))
   }
   tableHead.appendChild(tr('', columnHeaders))
@@ -45,8 +49,36 @@ const render = () => {
       let fieldName = `daily${_.capitalize(_state.selectedField)}Count`
       let dailyValues = prefecture[fieldName]
       for (let daysAgo of _.range(0, _state.maxDaysBefore)) {
-        let val = parseInt(dailyValues[dailyValues.length - 1 - daysAgo])
-        cells.push(cellWithValue('', val))
+        let totalValue = _.sum(_.slice(dailyValues, 0, dailyValues.length - 1 - daysAgo))
+        let dailyValue = parseInt(dailyValues[dailyValues.length - 1 - daysAgo])
+        let val = dailyValue
+        if (_state.countMethod == 'cumulative') {
+          val = totalValue
+        }
+      cells.push(cellWithValue('val', val))
+      }
+    } else if (_state.selectedField == 'recovered') {
+      let recoveries = _state.prefectureRecoveries[prefecture.name]
+      if (recoveries) {
+        for (let daysAgo of _.range(0, _state.maxDaysBefore)) {
+          const dateString = moment().subtract(daysAgo, 'days').format('YYYY-MM-DD')
+          const prevDateString = moment().subtract(daysAgo + 1, 'days').format('YYYY-MM-DD')
+          const totalValue = parseInt(recoveries[dateString])
+          const prevTotalValue = parseInt(recoveries[prevDateString])
+          if (isNaN(prevTotalValue)) {
+            prevTotalValue = 0
+          }
+          let dailyValue = totalValue - prevTotalValue
+          if (prefecture.name == 'Tokyo') {
+            console.log(prevDateString, prevTotalValue)
+          }
+
+          let val = dailyValue
+          if (_state.countMethod == 'cumulative') {
+            val = totalValue
+          }
+          cells.push(cellWithValue('val', val))
+        }
       }
     }
 
@@ -55,21 +87,33 @@ const render = () => {
   }
 }
 
-const process = (summary, patients) => {
+const process = (summary, patients, prefectureRecoveries) => {
   _state.patients = patients
   _state.prefectureSummary = summary.prefectures
   _state.dailySummary = summary.daily
-
-  console.log(patients.length)
+  _state.prefectureRecoveries = prefectureRecoveries
 }
 
 const initActions = () => {
-  document.querySelectorAll('.action').forEach(element => {
+  document.querySelectorAll('.action.field').forEach(element => {
     element.addEventListener('click', e => {
       e.preventDefault()
-      _state.selectedField = e.target.dataset.field
+      let target = e.target
+      _state.selectedField = target.dataset.field
       render()
-      e.target.classList.add('active')
+      document.querySelectorAll('.action.field.selected').forEach(element => { element.classList.remove('selected') })
+      target.classList.add('selected')
+    })
+  })
+
+  document.querySelectorAll('.action.countMethod').forEach(element => {
+    element.addEventListener('click', e => {
+      e.preventDefault()
+      let target = e.target
+      _state.countMethod = target.dataset.countMethod
+      render()
+      document.querySelectorAll('.action.countMethod.selected').forEach(element => { element.classList.remove('selected') })
+      target.classList.add('selected')
     })
   })
 }
@@ -80,13 +124,15 @@ const main = () => {
   const requests = [
     fetch('/patient_data/latest.json').then(response => response.json()),
     fetch('/summary/latest.json').then(response => response.json()),
+    fetch('/prefecture_recoveries.json').then(response => response.json()),
   ]
 
   Promise.all(requests)
     .then(responses => {
       const patients = responses[0]
       const summary = responses[1]
-      process(summary, patients)
+      const prefectureRecoveries = responses[2]
+      process(summary, patients, prefectureRecoveries)
       render()
     })
 }
