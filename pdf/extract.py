9b7080d67b9e6d0642b41ec1bdd02f961333e5b2
Alastair Tse <alastair@liquidx.net>
2020-12-10 20:22:16 +0900

    Testing programmatically updating the spreadsheet from python.

--- pdf/extract.py
+++ pdf/extract.py
@@ -13,6 +13,13 @@ import camelot
 import pandas as pd
 from bs4 import BeautifulSoup
 
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+
 def getLatestCovidReport(indexUrl):
   covidReportName = '新型コロナウイルス感染症の現在の状況'
   contents = urllib.request.urlopen(indexUrl).read()
@@ -64,10 +71,49 @@ def extractRecoveryNumbers(pdfPath):
 
   return [int(v.replace(',','')) for v in recovery]
 
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+SPREADSHEET_ID = '1vkw_Lku7F_F3F_iNmFFrDq9j7-tQ6EmZPOLpLt-s3TY'
+
+def writeValues(valueDate, values):
+  creds=None
+  if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+      creds = pickle.load(token)
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+        'credentials.json', SCOPES)
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+      pickle.dump(creds, token)
+
+  service = build('sheets', 'v4', credentials=creds)
+  sheet = service.spreadsheets()
+  result = sheet.values().get(spreadsheetId=SPREADSHEET_ID, range="'Sum By Day'!A2:G").execute()
+  if not result:
+    return False
+
+  values = result.get('values', [])
+  lastRow = values[-1]
+  if lastRow[0] == valueDate:
+    return False
+
+  #sheet.values().append(spreadsheetId=SPREADSHEET_ID, range="'Sum By Day'!A1:G").execute()
+  print(values[-1])
+
+
+
 if __name__ == '__main__':
   if len(sys.argv) > 1: 
     print(extractRecoveryNumbers(sys.argv[1]))
     sys.exit(0)
+
+  # writeValues('2020-12-09', None)
+  # sys.exit(0)
 
   indexUrl = 'https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000121431_00204.html'
   reportLink = getLatestCovidReport(indexUrl)
