f6c4d2b145f9cf3badff83e3cce1966d43d35acd
Alastair Tse <alastair@liquidx.net>
2020-12-12 18:58:06 +0900

    Extracts both text from the summary table and pdf

--- mhlw/extract.py
+++ mhlw/extract.py
@@ -8,10 +8,15 @@ import sys
 import re
 import urllib.request
 import tempfile
+import argparse
 
 import camelot
 import pandas as pd
 from bs4 import BeautifulSoup
+
+from PIL import Image
+import pytesseract
+
 
 import pickle
 import os.path
@@ -27,25 +32,28 @@ def getLatestCovidReport(indexUrl):
   links = soup.find_all('a')
   for link in links:
     if link and link.text and link.text.startswith(covidReportName):
-        return {
-          'title': link.text, 
-          'link': link['href']
-        }
+        return link['href']
   return None
 
-def getSummaryTableFromReportUrl(reportUrl):
-  contents = urllib.request.urlopen(reportUrl).read()
-  soup = BeautifulSoup(contents, features="html.parser")
+def getSummaryTable(soup):
   images = soup.find_all('img')
   for image in images:
     if image and image['src'] and image['src'].startswith('data'):
       return image['src']
   return None
 
-def getPdfDataFromReportUrl(reportUrl):
-  prefectureRecoveryName = '別紙１'
+def getReportFromUrl(reportUrl):
   contents = urllib.request.urlopen(reportUrl).read()
-  soup = BeautifulSoup(contents, features="html.parser")
+  return BeautifulSoup(contents, features="html.parser")
+
+def getReportDate(soup):
+  time = soup.find('time')
+  if time:
+    return time['datetime']
+  return None
+
+def getPdfData(soup):
+  prefectureRecoveryName = '別紙１'
   images = soup.find_all('img')
   links = soup.find_all('a')
   pdfLink = None
@@ -66,11 +74,52 @@ def extractRecoveryNumbers(pdfPath):
   tables = camelot.read_pdf(
       pdfPath, flavor='stream', pages='1')
 
-  recovery = tables[0].df.loc[5:, 5]
-  summary = tables[0].df.loc[5:, [0, 5]]
+  summary = tables[0].df.loc[5:, [0, 5]]  
 
-  return [int(v.replace(',','')) for v in recovery]
+  prefectureValues = []
+  for index, row in summary.iterrows():
+    prefecture = row[0]
+    prefecture = re.sub('※[0-9]', '', prefecture)
+    prefecture = re.sub('\s', '', prefecture)
 
+    value = row[5]
+    value = re.sub('※[0-9] ', '', value)
+    value = re.sub('[^0-9]+', '', value)
+    #print('%s:  %s' % (prefecture, value))
+    prefectureValues.append((prefecture, value))
+
+  # Strip last two rows
+  prefectureValues = prefectureValues[:-2]
+
+  return prefectureValues
+
+def extractImageAreas(image):
+  rowHeight = 16
+  pcrRect = (70, 140, 70 + 88, 140 + rowHeight)
+  criticalRect = (320, 140, 320 + 80, 140 + rowHeight)
+  portRecoveriesRect = (400, 84, 400 + 80, 84 + rowHeight)
+  recoveriesRect = (400, 140, 400 + 80, 140 + rowHeight)
+  deathsRect = (504, 140, 504 + 68, 140 + rowHeight)
+  return {
+    'pcr': image.crop(pcrRect),
+    'critical': image.crop(criticalRect),
+    'recoveries': image.crop(recoveriesRect),
+    'portRecoveries': image.crop(portRecoveriesRect),
+    'deaths': image.crop(deathsRect)
+  }
+
+def extractDailySummary(imageUrl):
+  imageData = urllib.request.urlopen(imageUrl)
+  image = Image.open(imageData)
+  subImages = extractImageAreas(image)
+  values = {}
+  for key in subImages:
+    subImage = subImages[key]
+    text = pytesseract.image_to_string(subImage)
+    num = int(text.replace(',', ''))
+    values[key] = num
+    #print('%s %d' % (key, num))
+  return values
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SPREADSHEET_ID = '1vkw_Lku7F_F3F_iNmFFrDq9j7-tQ6EmZPOLpLt-s3TY'
@@ -106,29 +155,56 @@ def writeValues(valueDate, values):
   print(values[-1])
 
 
-
 if __name__ == '__main__':
-  if len(sys.argv) > 1: 
-    print(extractRecoveryNumbers(sys.argv[1]))
-    sys.exit(0)
-
-  # writeValues('2020-12-09', None)
-  # sys.exit(0)
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--reportUrl')
+  parser.add_argument('--disableExtractRecoveries', action='store_true')
+  parser.add_argument('--extractSummary', action='store_true')
+  parser.add_argument('--outputText', action="store_true")
+  parser.add_argument('--writeResults', action='store_true')
+  args = parser.parse_args()
 
   indexUrl = 'https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/0000121431_00204.html'
-  reportLink = getLatestCovidReport(indexUrl)
+  reportUrl = None
+  if args.reportUrl:
+    reportUrl = args.reportUrl
+  else:
+    reportUrl = getLatestCovidReport(indexUrl)
+
+  reportDate = None
   reportPdfData = None
   tableImageData = None
+  
+  summaryValues = {}
 
-  if reportLink:
-    print(reportLink['title'])
-    print(reportLink['link'])
-    tableImageData = getSummaryTableFromReportUrl(reportLink['link'])
-    reportPdfData = getPdfDataFromReportUrl(reportLink['link'])
+  if reportUrl:
+    print(reportUrl)
+    reportSoup = getReportFromUrl(reportUrl)
+    reportDate = getReportDate(reportSoup)
+    summaryTableUrl = getSummaryTable(reportSoup)
+    reportPdfData = getPdfData(reportSoup)
+    print(reportDate)
 
-  if reportPdfData:
-    open('test.pdf', 'wb').write(reportPdfData)
-    with tempfile.NamedTemporaryFile(suffix='.pdf') as temp:
-      temp.write(reportPdfData)
-      numbers = extractRecoveryNumbers(temp.name)
-      [print(n) for n in numbers]
+  if not args.disableExtractRecoveries:
+    if reportPdfData:
+      open('test.pdf', 'wb').write(reportPdfData)
+      with tempfile.NamedTemporaryFile(suffix='.pdf') as temp:
+        temp.write(reportPdfData)
+        recoveries = extractRecoveryNumbers(temp.name)
+        summaryValues['prefectureRecoveries'] = recoveries
+
+  if args.extractSummary and summaryTableUrl:
+    values = extractDailySummary(summaryTableUrl)
+    summaryValues.update(values)
+
+  if args.outputText:
+    [print(v[1]) for v in summaryValues['prefectureRecoveries']]
+    print(summaryValues['portRecoveries'])
+    print('---')
+    print('recoveries,deaths,critical,tested')
+    print('%(recoveries)d\t%(deaths)d\t%(critical)d\t%(pcr)d' % summaryValues)
+
+  #print(summaryValues)
+
+  if args.writeResults:
+    writeValues(reportDate, summaryValues)
