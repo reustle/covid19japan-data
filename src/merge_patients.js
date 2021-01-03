1f8b2546dd55c912d0d21e411789616b7d74fad1
Alastair Tse <alastair@liquidx.net>
2021-01-03 22:24:25 +0900

    Temporarily disable

--- src/merge_patients.js
+++ src/merge_patients.js
@@ -5,7 +5,11 @@ const mergePatients = (allPatients) => {
   //const sortOrder = ['patientId', 'dateAnnounced']
   const sortOrder = ['dateAnnounced', 'patientId']
 
-  Verify.verifyPatients(allPatients)
+  try {
+    Verify.verifyPatients(allPatients)
+  } catch (e) {
+    console.warn(e)
+  }
   let patientsWithoutIds = _.sortBy(_.filter(allPatients, v => { return (v.patientId == -1)}), ['dateAnnounced'])
   let patientsWithIds = _.uniqBy(_.sortBy(_.filter(allPatients, v => { return (v.patientId != -1)}), sortOrder), 'patientId')
   return _.flatten([patientsWithIds, patientsWithoutIds])
